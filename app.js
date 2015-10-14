e68a0f9f1e2aa104bce7d694233bab55211487bc
Vanesa <vmartinnovella@yahoo.es>
2015-10-15 00:34:26 +0200

    añadidas las rutas

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var devices = require('./routes/devices');
+var providers = require('./routes/providers');
+
 
 var app = express();
 
@@ -21,9 +25,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session());
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.use('/devices',devices);
+app.use('/providers', providers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
