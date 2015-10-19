cc7d9580d5dd8a05fec61a826c4ac54316ccb534
Vanesa <vmartinnovella@yahoo.es>
2015-10-19 19:03:21 +0200

    Añadido formulario de login, boton en mapa

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 /*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+});*/
 
-router.get('/users', usersController.new); //formulario de sign up
+/*router.get('/users', usersController.new); //formulario de sign up
 router.get('/users', usersController.create);//crear un nuevo usuario
-*/
+x*/
 module.exports = router;
