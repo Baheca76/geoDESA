244132fabb6d740adb3c6bd83d66a2216f7adb3c
Sergio Sanchez <trukise@gmail.com>
2015-10-19 11:14:07 +0200

    Previo a cambios de Vanesa

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-//<<<<<<< HEAD
-  res.render('index', { title: 'geoDESA' });
-//=======
   res.render('index', { title: 'geoDESA, project by Vanesa Martín' });
-//>>>>>>> 8571293d5b7d7f93dec7dd416b92bf79a8d3accf
+
 });
 
 
