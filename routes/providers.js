1aaa17e154def9795b03fbb41be409ae463e5878
Vanesa <vmartinnovella@yahoo.es>
2015-11-10 23:05:25 +0100

    Añadido metodo delete de devices y provider

--- routes/providers.js
+++ routes/providers.js
@@ -9,7 +9,7 @@ router.param('providerId', providersController.load);
 router.get('/new', providersController.new);
 router.post('/create', providersController.create);
 router.get('/',providersController.list);
-router.delete('/delete/:id', providersController.delete);
+router.delete('/:providerId', providersController.delete);
 router.get('/:providerId', providersController.show);
 
 /*router.get('/providers/:id', providersController.list);
